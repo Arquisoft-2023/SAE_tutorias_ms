0fba39294eff7859bd713688348d3865f6ceaaba
Sebastian <sehernandezce@unal.edu.co>
2023-06-17 16:15:57 -0500

    URL db

--- src/config/Mongo.ts
+++ src/config/Mongo.ts
@@ -1,13 +1,13 @@
 import 'dotenv/config';
 import { connect } from 'mongoose';
-import { DB_TYPE, DB_HOST, DB_PORT, DB_NAME } from './Server';
-// import { DB_TYPE, DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_OTHER } from './Server';
+// import { DB_TYPE, DB_HOST, DB_PORT, DB_NAME } from './Server';
+import { DB_TYPE, DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_OTHER } from './Server';
 
 //const NODE_ENV = process.env.NODE_ENV;
 
 async function dbConnect(): Promise<void> {
-    const URI =`${DB_TYPE}://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
-    // const URI =`${DB_TYPE}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}${DB_OTHER}`;
+    // const URI =`${DB_TYPE}://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+    const URI =`${DB_TYPE}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}${DB_OTHER}`;
     await connect(URI);
 }
 
